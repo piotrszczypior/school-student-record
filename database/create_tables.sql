886683346b1c149456aa163b03065f8a097284f7
radziu2402 <radziu2402@gmail.com>
2023-12-10 13:40:38 +0100

    sorted tables

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,68 +1,71 @@
+CREATE TABLE "User" (
+  ID       SERIAL NOT NULL,
+  Email    varchar(150) NOT NULL UNIQUE,
+  Login    varchar(150) NOT NULL UNIQUE,
+  Role     varchar(50) NOT NULL,
+  Password varchar(50) NOT NULL,
+  PRIMARY KEY (ID)
+);
+
 CREATE TABLE Address (
-  ID              SERIAL NOT NULL, 
-  City            varchar(50) NOT NULL, 
-  Street          varchar(50) NOT NULL, 
-  "Post code"     varchar(10) NOT NULL, 
-  "Street number" int4 NOT NULL, 
-  "Flat number"   int4 NOT NULL, 
+  ID              SERIAL NOT NULL,
+  City            varchar(50) NOT NULL,
+  Street          varchar(50) NOT NULL,
+  "Post code"     varchar(10) NOT NULL,
+  "Street number" int4 NOT NULL,
+  "Flat number"   int4 NOT NULL,
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE Administrator (
-  "First name" varchar(50) NOT NULL, 
-  "Last name"  varchar(50) NOT NULL, 
-  UsersID      int4 NOT NULL, 
+  "First name" varchar(50) NOT NULL,
+  "Last name"  varchar(50) NOT NULL,
+  UsersID      int4 NOT NULL,
   PRIMARY KEY (UsersID),
   CONSTRAINT FK_Administrator_User FOREIGN KEY (UsersID) REFERENCES "User" (ID)
 );
 
-CREATE TABLE Attendance (
-  ID             SERIAL NOT NULL, 
-  "Date"         date NOT NULL, 
-  Present        bool NOT NULL, 
-  CreatedBy      int4 NOT NULL,
-  LessonsID      int4 NOT NULL, 
-  StudentUsersID int4 NOT NULL, 
-  PRIMARY KEY (ID),
-  CONSTRAINT FK_Attendance_Lesson FOREIGN KEY (LessonsID) REFERENCES Lesson (ID),
-  CONSTRAINT FK_Attendance_Student FOREIGN KEY (StudentUsersID) REFERENCES Student (UsersID)
+CREATE TABLE Class (
+  ID   SERIAL NOT NULL,
+  Name varchar(50) NOT NULL,
+  PRIMARY KEY (ID)
 );
 
-CREATE TABLE Class (
-  ID   SERIAL NOT NULL, 
-  Name varchar(50) NOT NULL, 
+CREATE TABLE Subject (
+  ID   SERIAL NOT NULL,
+  Name varchar(50) NOT NULL,
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE Classroom (
-  ID          SERIAL NOT NULL, 
-  "Room name" varchar(50) NOT NULL, 
+  ID          SERIAL NOT NULL,
+  "Room name" varchar(50) NOT NULL,
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE Grade (
-  ID                     SERIAL NOT NULL, 
-  "Grade value"          numeric(3, 2) NOT NULL, 
-  SubjectsID             int4 NOT NULL, 
-  "Date of modification" date NOT NULL, 
-  CreatedBy              int4 NOT NULL,
-  TeacherUsersID         int4 NOT NULL, 
-  StudentUsersID         int4 NOT NULL, 
-  PRIMARY KEY (ID),
-  CONSTRAINT FK_Grade_Subject FOREIGN KEY (SubjectsID) REFERENCES Subject (ID),
-  CONSTRAINT FK_Grade_Student FOREIGN KEY (StudentUsersID) REFERENCES Student (UsersID),
-  CONSTRAINT FK_Grade_Teacher FOREIGN KEY (TeacherUsersID) REFERENCES Teacher (UsersID)
+CREATE TABLE Teacher (
+  "First name"      varchar(50) NOT NULL,
+  "Last name"       varchar(50) NOT NULL,
+  "Date of birth"   date NOT NULL,
+  "Academic degree" varchar(50),
+  UsersID           int4 NOT NULL,
+  ClassID           int4,
+  AddressID         int4 NOT NULL,
+  PRIMARY KEY (UsersID),
+  CONSTRAINT FK_Teacher_Class FOREIGN KEY (ClassID) REFERENCES Class (ID),
+  CONSTRAINT FK_Teacher_Address FOREIGN KEY (AddressID) REFERENCES Address (ID),
+  CONSTRAINT FK_Teacher_User FOREIGN KEY (UsersID) REFERENCES "User" (ID)
 );
 
 CREATE TABLE Lesson (
-  ID             SERIAL NOT NULL, 
-  Topic          varchar(50), 
+  ID             SERIAL NOT NULL,
+  Topic          varchar(50),
   CreatedBy      int4 NOT NULL,
-  "Date"         date NOT NULL, 
-  ClassesID      int4 NOT NULL, 
-  SubjectsID     int4 NOT NULL, 
-  ClassroomID    int4 NOT NULL, 
-  TeacherUsersID int4 NOT NULL, 
+  "Date"         date NOT NULL,
+  ClassesID      int4 NOT NULL,
+  SubjectsID     int4 NOT NULL,
+  ClassroomID    int4 NOT NULL,
+  TeacherUsersID int4 NOT NULL,
   PRIMARY KEY (ID),
   CONSTRAINT FK_Lesson_Class FOREIGN KEY (ClassesID) REFERENCES Class (ID),
   CONSTRAINT FK_Lesson_Subject FOREIGN KEY (SubjectsID) REFERENCES Subject (ID),
@@ -71,51 +74,51 @@ CREATE TABLE Lesson (
 );
 
 CREATE TABLE Student (
-  "First name"    varchar(50) NOT NULL, 
-  "Last name"     varchar(50) NOT NULL, 
-  "Date of birth" date NOT NULL, 
-  ClassesID       int4 NOT NULL, 
-  UsersID         int4 NOT NULL, 
-  AddressID       int4 NOT NULL, 
+  "First name"    varchar(50) NOT NULL,
+  "Last name"     varchar(50) NOT NULL,
+  "Date of birth" date NOT NULL,
+  ClassesID       int4 NOT NULL,
+  UsersID         int4 NOT NULL,
+  AddressID       int4 NOT NULL,
   PRIMARY KEY (UsersID),
   CONSTRAINT FK_Student_Class FOREIGN KEY (ClassesID) REFERENCES Class (ID),
   CONSTRAINT FK_Student_Address FOREIGN KEY (AddressID) REFERENCES Address (ID),
   CONSTRAINT FK_Student_User FOREIGN KEY (UsersID) REFERENCES "User" (ID)
 );
 
-CREATE TABLE Subject (
-  ID   SERIAL NOT NULL, 
-  Name varchar(50) NOT NULL, 
-  PRIMARY KEY (ID)
+CREATE TABLE Attendance (
+  ID             SERIAL NOT NULL,
+  "Date"         date NOT NULL,
+  Present        bool NOT NULL,
+  CreatedBy      int4 NOT NULL,
+  LessonsID      int4 NOT NULL,
+  StudentUsersID int4 NOT NULL,
+  PRIMARY KEY (ID),
+  CONSTRAINT FK_Attendance_Lesson FOREIGN KEY (LessonsID) REFERENCES Lesson (ID),
+  CONSTRAINT FK_Attendance_Student FOREIGN KEY (StudentUsersID) REFERENCES Student (UsersID)
+);
+
+CREATE TABLE Grade (
+  ID                     SERIAL NOT NULL,
+  "Grade value"          numeric(3, 2) NOT NULL,
+  SubjectsID             int4 NOT NULL,
+  "Date of modification" date NOT NULL,
+  CreatedBy              int4 NOT NULL,
+  TeacherUsersID         int4 NOT NULL,
+  StudentUsersID         int4 NOT NULL,
+  PRIMARY KEY (ID),
+  CONSTRAINT FK_Grade_Subject FOREIGN KEY (SubjectsID) REFERENCES Subject (ID),
+  CONSTRAINT FK_Grade_Student FOREIGN KEY (StudentUsersID) REFERENCES Student (UsersID),
+  CONSTRAINT FK_Grade_Teacher FOREIGN KEY (TeacherUsersID) REFERENCES Teacher (UsersID)
 );
 
 CREATE TABLE Subjects_Teachers (
-  SubjectsID     int4 NOT NULL, 
-  TeacherUsersID int4 NOT NULL, 
+  SubjectsID     int4 NOT NULL,
+  TeacherUsersID int4 NOT NULL,
   PRIMARY KEY (SubjectsID, TeacherUsersID),
   CONSTRAINT FK_SubjectsTeachers_Subject FOREIGN KEY (SubjectsID) REFERENCES Subject (ID),
   CONSTRAINT FK_SubjectsTeachers_Teacher FOREIGN KEY (TeacherUsersID) REFERENCES Teacher (UsersID)
 );
 
-CREATE TABLE Teacher (
-  "First name"      varchar(50) NOT NULL, 
-  "Last name"       varchar(50) NOT NULL, 
-  "Date of birth"   date NOT NULL, 
-  "Academic degree" varchar(50), 
-  UsersID           int4 NOT NULL, 
-  ClassID           int4, 
-  AddressID         int4 NOT NULL, 
-  PRIMARY KEY (UsersID),
-  CONSTRAINT FK_Teacher_Class FOREIGN KEY (ClassID) REFERENCES Class (ID),
-  CONSTRAINT FK_Teacher_Address FOREIGN KEY (AddressID) REFERENCES Address (ID),
-  CONSTRAINT FK_Teacher_User FOREIGN KEY (UsersID) REFERENCES "User" (ID)
-);
 
-CREATE TABLE "User" (
-  ID       SERIAL NOT NULL, 
-  Email    varchar(150) NOT NULL UNIQUE, 
-  Login    varchar(150) NOT NULL UNIQUE, 
-  Role     varchar(50) NOT NULL, 
-  Password varchar(50) NOT NULL, 
-  PRIMARY KEY (ID)
-);
+
